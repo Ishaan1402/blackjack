639b2852de73ff912f30a79c2dcce989e3c940aa
Ishaan <ishbhagat02@gmail.com>
2024-07-12 15:24:19 -0400

    initial commit

---
+++ src/test/java/com/ishaan1402/blackjack/BlackjackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishaan1402.blackjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
